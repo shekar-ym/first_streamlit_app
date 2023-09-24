33975a8d3693a450818d9f8386b3942c0f27236a
Chandra <shekar.ym@gmail.com>
2023-09-24 15:57:18 +1000

    Update streamlit_app.py
    second commit - menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title("Healthy dinner menu for customers")
+
+streamlit.header("Breakfast menu")
+
+streamlit.text("Kale, Apple, Rocket Smoothie")
+streamlit.text("Avocado Sour dough toast")
+streamlit.text("Scrambled Eggs")
